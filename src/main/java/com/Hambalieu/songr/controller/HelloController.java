7e8ca162b8074eaed7e1ed44815b57aaf148168f
Hambalieu <hambalieujallow@gmail.com>
2022-02-28 22:43:30 -0800

    completed lab 11

---
+++ src/main/java/com/Hambalieu/songr/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.Hambalieu.songr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloMethod(){
+        return "hello.html";
+    }
+
+    @GetMapping("/capitalize/{words}")
+    public String getCapitalize(@PathVariable String words, Model m) {
+        m.addAttribute("words", setCapitalize(words));
+        return "capitalize.html";
+    }
+
+    public static String setCapitalize(String words) {
+        return words.toUpperCase();
+    }
+
+    @GetMapping("/")
+    public String getRootPage(){
+        return"index.html";
+    }
+
+}
